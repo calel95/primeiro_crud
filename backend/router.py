e8d4b2b61276e9ca913e2f3b40edc867a2c82e78
Calel95 <borgescalel@gmail.com>
2024-09-09 20:00:32 -0300

    adicionado frontend, mas nao funcionando

--- backend/router.py
+++ backend/router.py
@@ -5,7 +5,7 @@ from typing import List
 
 router = APIRouter()
 
-@router.get("/produtos/", response_model=List[schemas.ProductGet], description="Faz a leitura de todos os registros armazenados no banco")
+@router.get("/produtos/", response_model=List[schemas.ProductGet], description="Faz a leitura de todos os produtos armazenados no banco")
 def read_all_products(db: Session = Depends(database.get_db)):
     products = controller.get_products(db)
     return products
@@ -17,19 +17,19 @@ def read_one_product(produto_id: int, db: Session = Depends(database.get_db)):
         raise HTTPException(status_code=404, detail="Produto com o id {produto_id} não encontrado")
     return db_product
 
-@router.post("/produtos", response_model=schemas.ProductBase)
-def create_product(produto: schemas.ProductBase ,db: Session = Depends(database.get_db), description="Faz a criação de um novo produto"):
+@router.post("/produtos/", response_model=schemas.ProductBase, description="Faz a criação de um novo produto")
+def create_product(produto: schemas.ProductBase ,db: Session = Depends(database.get_db)):
     db_product = controller.create_product(db=db,produto=produto)
     return db_product
 
-@router.delete("/produtos/{produto_id}", response_model=schemas.ProductGet)
+@router.delete("/produtos/{produto_id}", response_model=schemas.ProductGet, description="Faz a deleção de um produto")
 def delete_product(produto_id: int, db: Session = Depends(database.get_db)):
     db_product = controller.delete_product(db=db,produto_id=produto_id)
     if db_product is None:
         raise HTTPException(status_code=404, detail="Produto com o id {produto_id} não encontrado")
     return db_product
 
-@router.put("/produtos/{produto_id}",response_model=schemas.ProductGet)
+@router.put("/produtos/{produto_id}",response_model=schemas.ProductGet, description="Faz a atualização de um produto")
 def update_products(produto_id: int, produto: schemas.ProductUpdate, db: Session = Depends(database.get_db)):
     db_product = controller.update_product(db=db, produto_id=produto_id, produto=produto)
     if db_product is None:
